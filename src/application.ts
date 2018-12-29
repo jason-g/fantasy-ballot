5f9f082e90f271bf5b3147d5cad464848c61a108
Jason G <jason@jroid.com>
2018-12-29 00:17:45 -0500

    adding oasgraph and coresonding server for graphQL endpoints

--- src/application.ts
+++ src/application.ts
@@ -1,19 +1,26 @@
-import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
+import { BootMixin } from '@loopback/boot';
+import { ApplicationConfig } from '@loopback/core';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
-import {ServiceMixin} from '@loopback/service-proxy';
+import { RepositoryMixin } from '@loopback/repository';
+import { RestApplication } from '@loopback/rest';
+import { ServiceMixin } from '@loopback/service-proxy';
 import * as path from 'path';
-import {MySequence} from './sequence';
+import { MySequence } from './sequence';
 
 export class FantasyBallotApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
+    options = Object.assign({}, {
+      rest: {
+        openApiSpec: {
+          servers: [{ url: 'http://localhost:3000' }],
+        }
+      }
+    });
     super(options);
 
     // Set up the custom sequence
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import {FantasyBallotApplication} from './application';
-import {ApplicationConfig} from '@loopback/core';
+import { FantasyBallotApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
 
-export {FantasyBallotApplication};
+const OASGraph = require('oasgraph');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+export { FantasyBallotApplication };
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new FantasyBallotApplication(options);
@@ -12,5 +16,34 @@ export async function main(options: ApplicationConfig = {}) {
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
 
+  try {
+    const { schema } = await
+      OASGraph.createGraphQlSchema(
+        app.restServer.getApiSpec(),
+        {
+          strict: false,
+          viewer: true,
+          addSubOpperations: true,
+          sendOAuthTokeninQuery: false,
+        },
+      );
+
+    //set u express server for graphQL
+    const myExpress = express();
+    myExpress.use('/graphql', graphqlHTTP(
+      {
+        schema: schema,
+        graphiql: true,
+      }
+    ));
+    myExpress.listen(3001, () => {
+      console.log('GraphQL up at http://localhost:3001/graphql');
+    });
+  } catch (err) {
+    console.log('Error starting graphQL:', err.message);
+  }
+  //const { createGraphQlSchema } = require('oasgraph')
+  // load or construct OAS (const oas = ...)
+  //const { schema, report } = await createGraphQlSchema('http://127.0.0.1:3000/openapi.json');
   return app;
 }
