3cc0ce322a1830da0b3c0cced8b40cc6634a7b33
Jason G <jason@jroid.com>
2018-12-30 20:45:46 -0500

    wire in CORS for graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { FantasyBallotApplication } from './application';
 import { ApplicationConfig } from '@loopback/core';
+import * as cors from "cors";
 
 const OASGraph = require('oasgraph');
 const express = require('express');
@@ -30,12 +31,26 @@ export async function main(options: ApplicationConfig = {}) {
 
     //set u express server for graphQL
     const myExpress = express();
+    //options for cors midddleware
+    const API_URL = "http://lvh.me:3006";
+    const options: cors.CorsOptions = {
+      allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+      credentials: true,
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      origin: API_URL,
+      preflightContinue: false
+    };
+    myExpress.use(cors(options));
+
     myExpress.use('/graphql', graphqlHTTP(
       {
         schema: schema,
-        graphiql: true,
+        graphiql: true
       }
     ));
+
+    // cors preflight
+    myExpress.options("*", cors(options));
     myExpress.listen(3001, () => {
       console.log('GraphQL up at http://localhost:3001/graphql');
     });
